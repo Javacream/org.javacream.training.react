2cb834e8a537481693ddc36d51a8358c747b5aba
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-03-26 15:32:27 +0200

    add collections example: sort, map, forEach

--- public/js/PeopleApp.js
+++ public/js/PeopleApp.js
@@ -1,6 +1,6 @@
 let p1 = new Person(1, "Meier", "Avery")
-let p2 = new Student(2, "Metzger", "Carol", "LMU")
-let p3 = new Worker(3, "Mauser", "Joyce", "Integrata")
+let p2 = new Student(2, "Metzger", "Joyce", "LMU")
+let p3 = new Worker(3, "Mauser", "Carol", "Integrata")
 
 dump()
 p1.marry(p2)
@@ -16,3 +16,18 @@ function dump(){
   console.log(p2.info())
   console.log(p3.info())
 }
+
+let people = []
+people.push(p1, p2, p3)
+
+
+console.log("************** SORT LASTNAME**************")
+people.sort((pa, pb) => {return pa.lastname.localeCompare(pb.lastname)} )
+people.forEach((p) => console.log(p.info()))
+console.log("************** SORT FIRSTNAME**************")
+people.sort((pa, pb) => {return pa.firstname.localeCompare(pb.firstname)} )
+people.forEach((p) => console.log(p.info()))
+
+console.log("************** PEOPLE INFOS**************")
+let peopleInfos = people.map((p) => p.info())
+console.log(...peopleInfos)
