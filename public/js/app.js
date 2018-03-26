765778d3e37c75f927629a6c7135cb5e11a8e819
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-03-26 15:32:50 +0200

    add PeopleComponent, PersonComponent

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,21 @@
 class WelcomeComponent extends React.Component{
   render(){
-    return <p>Welcome to the People App!</p>
+    return <h2>Welcome to the People App!</h2>
   }
 }
 
-ReactDOM.render(<WelcomeComponent />, document.getElementById('content'))
+class People extends React.Component{
+  render(){
+    let peopleInfoHtml = people.map((p) =>
+    <Person key={p.personId} info={p.info()} />)
+  return   <div className='ui unstackable items'>{peopleInfoHtml}</div>
+}
+}
+class Person extends React.Component{
+  render(){
+    return <li>{this.props.info}</li>
+  }
+}
+
+ReactDOM.render(<WelcomeComponent />, document.getElementById('welcome-content'))
+ReactDOM.render(<People />, document.getElementById('people'))
