4e392fa1e831e070ad7c69cfd7134c737bfc340b
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-03-26 13:35:39 +0200

    add simple react component

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
+class WelcomeComponent extends React.Component{
+  render(){
+    return <p>Welcome to the People App!</p>
+  }
+}
 
+ReactDOM.render(<WelcomeComponent />, document.getElementById('content'))
