df6896be1057aae5d12bb45a0f389774414c09a7
Rainer Sawitzki <training@rainer-sawitzki>
2019-01-16 22:47:02 +0100

    initial project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ProductList extends React.Component {
+render() {
+    return (
+    <div className='ui unstackable items'>
+    Hello, friend! I am a basic React component.
+    </div>
+    );
+    }
+}
+ReactDOM.render(
+<ProductList />,
+document.getElementById('content')
+);
