f6f9bbb1a4abd9b8a46d97398288cc01e435716f
Rainer Sawitzki <training@rainer-.sawitzki.de>
2019-02-26 13:20:27 +0100

    add clock

--- people-ts/src/App.tsx
+++ people-ts/src/App.tsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {StatelessPersonSampleDataComponent} from './PeopleSampleDataComponent'
+import {ClockComponent} from './ClockComponent'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <hr />
+          <ClockComponent />
           <hr />
           <StatelessPersonSampleDataComponent />
         </header>
