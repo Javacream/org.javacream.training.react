a8d7b33a554dc4149a70c58c77966c1e99adfd33
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-10-07 16:38:26 +0200

    add PeopleComponent

--- training/src/people/component/PeopleComponent.js
+++ training/src/people/component/PeopleComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {peopleData} from '../testdata/PeopleData'
 function PeopleComponent() {
-    const peopleHtml = peopleData.allInfos().map( (info) => <p>{info}</p>)
+    const peopleHtml = peopleData.allPeopleBy('m').map( (p) => <li>{p.info()}</li>)
     return (
         <div><ol>{peopleHtml}</ol></div>    
     );
