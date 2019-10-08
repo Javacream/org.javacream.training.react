08f4e3b7239e6f90b6ec404ae5d63c394150b9e5
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-10-08 12:01:29 +0200

    add hook example

--- training/src/App.js
+++ training/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import PeopleHeaderComponent from './main/component/PeopleHeaderComponent'
 import PeopleFooterComponent from './main/component/PeopleFooterComponent'
-import PeopleAppComponent from './people/component/PeopleAppComponent'
+import PeopleAppComponent from './people/component/PeopleAppHookComponent'
 function App() {
   return (
     <div className="App">
