5b664e5408782f8a8ce217fed403672042f97dfd
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-10-09 08:17:15 +0200

    add server example

--- training/src/App.js
+++ training/src/App.js
@@ -5,12 +5,15 @@ import PeopleFooterComponent from './main/component/PeopleFooterComponent'
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import PeopleAppComponent from './people/component/PeopleAppHookComponent'
 import PeopleAppComponent from './people/component/PeopleAppComponent'
+import PeopleFromServerComponent from './people/component/PeopleFromServerComponent'
 import LoggingComponent from './logging/component/LoggingComponent'
 function App() {
   return (
     <div className="App">
     <PeopleHeaderComponent />
     <PeopleAppComponent />
+    <hr />
+    <PeopleFromServerComponent />
     <LoggingComponent numberOfMessages="3" />
     <PeopleFooterComponent />
     </div>
