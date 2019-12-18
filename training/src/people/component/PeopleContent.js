6c86ffa430c905ecbf7caed8d24cd6f98f7a31c2
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-12-18 09:59:18 +0100

    add PersonSearchComponent

--- training/src/people/component/PeopleContent.js
+++ training/src/people/component/PeopleContent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PeopleComponent from './PeopleComponent'
 import PersonInputFormComponent from "./PeopleInputFormComponent"
+import PersonSearchComponent from "./PersonSearchComponent"
 import PersonFromServerComponent from "./PeopleFromServerComponent"
 export default function PeopleContent(props){
     return (
@@ -8,6 +9,8 @@ export default function PeopleContent(props){
         <PeopleComponent people={props.people}/>
         <hr />
         <PersonInputFormComponent createPersonHandler={props.createPersonHandler}/>
+        <hr />
+        <PersonSearchComponent />
         <hr />
         <PersonFromServerComponent />
         </>
