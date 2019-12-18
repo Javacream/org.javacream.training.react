4f4450a014079b82b51834169df298d502bac818
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-12-18 08:06:17 +0100

    add client server

--- training/src/people/component/PeopleContent.js
+++ training/src/people/component/PeopleContent.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PeopleComponent from './PeopleComponent'
 import PersonInputFormComponent from "./PeopleInputFormComponent"
+import PersonFromServerComponent from "./PeopleFromServerComponent"
 export default function PeopleContent(props){
     return (
         <>
         <PeopleComponent people={props.people}/>
         <hr />
         <PersonInputFormComponent createPersonHandler={props.createPersonHandler}/>
+        <hr />
+        <PersonFromServerComponent />
         </>
         )
 }
