94cacf9561a6b85613f4ca361c15e706ecb245f8
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-08-18 14:12:26 +0200

    start HookClockComponent

--- training/src/people/components/FooterComponent.tsx
+++ training/src/people/components/FooterComponent.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {config} from '../ApplicationContext'
-import ClockComponent from '../../util/ClockComponent'
+import {ClockComponent, HookClockComponent} from '../../util/ClockComponent'
 export default function FooterComponent() {
     return  (
         <div id="footer">
             <p className="right">{config.company}</p>
             <ClockComponent />
+            <HookClockComponent />
         </div>
       )
 
--- training/src/util/ClockComponent.tsx
+++ training/src/util/ClockComponent.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default class ClockComponent extends React.PureComponent{
+export class ClockComponent extends React.PureComponent{
     state = {actualDate: new Date()}
     componentDidMount(){
         setInterval( () => {this.setState({actualDate: new Date()})}, 1000)
@@ -12,4 +12,14 @@ export default class ClockComponent extends React.PureComponent{
             </div>
         )
     }
+}
+
+export function HookClockComponent(){
+    const [date, updateDate] = useState({actualDate: new Date()})
+    //setInterval( () => {updateDate({actualDate: new Date()})}, 1000)
+    return (
+        <div>
+            <p>{date.actualDate.toLocaleString()}</p>
+        </div>
+    )
 }
