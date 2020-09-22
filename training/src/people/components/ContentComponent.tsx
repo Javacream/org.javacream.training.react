4a1793bb56784d6decd21d710b4cc9305199bfe5
= <=>
2020-09-22 09:52:08 +0200

    ContentComponent as stateful functional component

--- training/src/people/components/ContentComponent.tsx
+++ training/src/people/components/ContentComponent.tsx
@@ -1,27 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PeopleListComponent from './PeopleListComponent'
 import {peopleModel} from './PeopleApplicationContext'
-import { Person } from '../model/PeopleModel';
 import PersonInputComponent from './PersonInputComponent'
-export class ContentComponent extends React.PureComponent {
-    state = {people: new Array<Person>()}
-    componentDidMount(){
-      this.setState({people: peopleModel.findAll()})
-    }
 
-    handlePersonCreation = (lastname:string, firstname:string, gender:string, height:number) =>{
+export function ContentComponent() {
+
+    let [people, updatePeople] = useState(peopleModel.findAll())
+    const handlePersonCreation = (lastname:string, firstname:string, gender:string, height:number) =>{
       peopleModel.create(lastname, firstname, gender, height)
-      this.setState({people: peopleModel.findAll()})
-}
-    render(){
-      return  (
-        <>
-          <PeopleListComponent people={this.state.people}/>
-          <hr />
-          <PersonInputComponent callback={this.handlePersonCreation}/>
-
-        </>
+      updatePeople(peopleModel.findAll())
+    }
+    return  (
+       <>
+         <PeopleListComponent people={people}/>
+         <hr />
+         <PersonInputComponent callback={handlePersonCreation}/>
+       </>
       )
-}
-
 }
