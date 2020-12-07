defc4170a34cacb8e54e93d963b8629784477735
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-12-07 13:53:09 +0100

    play around with functions

---
+++ training/src/people/people.tsx
@@ -0,0 +1,11 @@
+export let appTitle = "P E O P L E"
+
+//classic function
+export function doSomething() {
+    return "OK"
+}
+
+//Arrow-Syntax
+export let doSomethingArrow = () => {
+    return "OK"
+}
