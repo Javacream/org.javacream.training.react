c819f09ba53a12ee8a0394ea393f2de82ae2305a
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-12-07 14:17:33 +0100

    type inference

--- training/src/people/people.tsx
+++ training/src/people/people.tsx
@@ -1,7 +1,7 @@
 export let appTitle = "P E O P L E"
-
+let demo = [1, 2]
 //classic function
-export function doSomething() {
+export function doSomething(){
     return "OK"
 }
 
@@ -9,3 +9,29 @@ export function doSomething() {
 export let doSomethingArrow = () => {
     return "OK"
 }
+let funcWithParam: (x:boolean)=> string|number = (p:boolean): string | number=> {
+    if(p){
+        return "OK"
+    }else{
+        return 42
+    }
+}
+
+//appTitle = 42
+
+
+let result = funcWithParam(true)
+
+function demoWithCallback(callBackFunction: (x:string) => boolean){
+    return callBackFunction("Hugo")
+}
+
+
+let r = demoWithCallback( (p:string) => {
+    return true
+})
+/*
+let r = demoWithCallback( (p:number) => {
+    return true
+})
+*/
