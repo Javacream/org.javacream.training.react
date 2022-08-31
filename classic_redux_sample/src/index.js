1dee00abc0915a5951f21501773a50871cfae40b
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-08-31 13:12:37 +0200

    add debug middleware

--- classic_redux_sample/src/index.js
+++ classic_redux_sample/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, composeEnhancers(applyMiddleware()))
+
 
 render(
   <Provider store={store}>
