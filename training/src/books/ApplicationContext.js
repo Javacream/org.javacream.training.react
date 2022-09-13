e09e9e3e2b4bf67a1a941de3ac76249f58eac3bc
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-09-13 15:57:42 +0200

    implement BookList component

--- training/src/books/ApplicationContext.js
+++ training/src/books/ApplicationContext.js
@@ -1,6 +1,14 @@
+import Book from "./model/Book";
 import BooksRepository from "./model/BooksRepository";
 
 const booksRepository = new BooksRepository()
 
+for (let i = 1000; i < 1005; i++){
+    const isbn = "ISBN" + i
+    const title = "TITLE" + i
+    const price = 1.99 * i
+    const available = true
+    booksRepository.books.set(isbn, new Book(isbn, title, price, available))
 
+}
 export {booksRepository}
--- training/src/books/components/BooksList.js
+++ training/src/books/components/BooksList.js
@@ -1,9 +1,13 @@
-import booksRepository from "../ApplicationContext"
+import {booksRepository} from "../ApplicationContext"
 
 export default function BooksList(){
     //const booksRepository = new BooksRepository()
-    const booksList = booksRepository.findAll()
+    const booksList = booksRepository.findAll()//booksList = Array<Book>
+    const booksListHtml = booksList.map((book) => <><p>{book.isbn}</p></>) //booksListHtml = Array<HTML>
     return (
-        <><p>BOOKS LIST</p></>
+        <>
+            <p>BOOKS LIST</p>
+            {booksListHtml}
+        </>
     )
 }
