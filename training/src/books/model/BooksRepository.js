0ee6da876dc1808b8f6bdd418c6fd59baa91e8b2
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-09-13 15:01:00 +0200

    start BooksRepository

---
+++ training/src/books/model/BooksRepository.js
@@ -0,0 +1,31 @@
+import Book from './Book'
+export default class BooksRepository{
+    constructor(){
+        this.books = new Map()
+        this.counter = 0
+    }
+
+    create(title){
+        const isbn = "ISBN" + this.counter++
+        const newBook = new Book(isbn, title, 19.99, false)
+        this.books.set(isbn, newBook)
+    }
+
+    findAll(){
+        return Array.from(this.books.values())
+    }
+
+    findByIsbn(isbn){
+        return this.books.get(isbn)
+    }
+
+    updatePrice(isbn, newPrice){
+        this.books.get(isbn).price = newPrice
+    }
+    updateAvailability(isbn, available){
+        this.books.get(isbn).available = available
+    }
+    deleteByIsbn(isbn){
+        delete this.books.isbn
+    }
+}
