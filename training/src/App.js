c6f07b38359f43e9deac2293268a99ccaee1b6a1
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-07-12 12:15:34 +0200

    play with app

--- training/src/App.js
+++ training/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <p>Hello React</p>
+export let name ="Hugo"
+export function App() {
+  let object = 42  //object ist eine Referenz auf ein Objekt. Das Objekt wurde durch das Literal 42 auf eine number mit Wert 42 gesetzt
+  object = "Hugo" //object ist nun eine Referenz auf das String-Objekt, das mit dem Literal "..." ...
+  object = (
+    <>
+      <div>
+        <p>Hello React</p>
+      </div>
+      <div>
+      <p>Hello World</p>
     </div>
-  );
+    </>
+) //Das ist das HTML-Literal
+  return object;
 }
 
-export default App;
--- training/src/index.js
+++ training/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+import {App, name} from './App' //{name} = {name: name}
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
