bf8a49de0b81b0d9c64735b75e217b21ed9b50b4
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-07-12 13:37:38 +0200

    add static PeopleApp

--- training/src/App.js
+++ training/src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
-
-export let name ="Hugo"
-export function App() {
-  let object = 42  //object ist eine Referenz auf ein Objekt. Das Objekt wurde durch das Literal 42 auf eine number mit Wert 42 gesetzt
-  object = "Hugo" //object ist nun eine Referenz auf das String-Objekt, das mit dem Literal "..." ...
-  object = (
-    <>
-      <div>
-        <p>Hello React</p>
-      </div>
-      <div>
-      <p>Hello World</p>
-    </div>
-    </>
-) //Das ist das HTML-Literal
-  return object;
+import PeopleApp from './people/PeopleApp'
+export default function App() {
+  return (
+    <PeopleApp></PeopleApp>
+      
+);
 }
 
--- training/src/index.js
+++ training/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
-import {App, name} from './App' //{name} = {name: name}
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
