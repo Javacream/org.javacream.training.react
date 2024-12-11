27a5fca8a01ed3e918391039b00c46137bd4c1c3
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-12-11 16:00:55 +0100

    Solution Slide 20

--- training/src/people/components/PeopleList.jsx
+++ training/src/people/components/PeopleList.jsx
@@ -2,10 +2,10 @@ import {peopleModel} from '../PeopleContext'
 import PersonComponent from './PersonComponent'
 export default function PeopleList(){
     const peopleList = peopleModel.allPeople()
-    const peopleHtmlList = peopleList.map((p) => <PersonComponent key={p.id} person_param={p}/>)
+    const peopleHtmlList = peopleList.map((p) => <li><PersonComponent key={p.id} person_param={p} detail={true}/></li>)
     return (
-        <>
+        <ul>
             {peopleHtmlList}
-        </>
+        </ul>
     )
 }
--- training/src/people/components/PersonComponent.jsx
+++ training/src/people/components/PersonComponent.jsx
@@ -1,8 +1,19 @@
 export default function PersonComponent(props){
     let aPerson = props.person_param
-    return (
-        <>
-            <p>{aPerson.firstname} {aPerson.lastname} {aPerson.height}</p>
-        </>
-    )
+    let detail = props.detail
+    if (detail) {
+        return (
+            <>
+                <div>{aPerson.firstname} {aPerson.lastname} {aPerson.height} {aPerson.gender}</div>
+            </>
+        )
+    }
+    else{
+        return (
+            <>
+                <div>{aPerson.firstname} {aPerson.lastname}</div>
+            </>
+        )
+
+    }
 }
