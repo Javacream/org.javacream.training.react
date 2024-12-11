3b2ae54be48a28a66a152b55bafd2b3babc83872
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-12-11 15:26:58 +0100

    add PeopleList component

--- training/src/people/PeopleContext.jsx
+++ training/src/people/PeopleContext.jsx
@@ -1,2 +1,8 @@
+import { PeopleModel } from "./model/PeopleModel"
+
 export const applicationTitle = 'PEOPLE'
 export const company = 'Javacream'
+export let peopleModel = new PeopleModel()
+peopleModel.create('Doe', 'Johanna', 176, 'f')
+peopleModel.create('Sawitzki', 'Rainer', 183, 'm')
+peopleModel.create('Smith', 'Andrea', 156, 'd')
---
+++ training/src/people/components/PeopleList.jsx
@@ -0,0 +1,10 @@
+import {peopleModel} from '../PeopleContext'
+export default function PeopleList(){
+    const peopleList = peopleModel.allPeople()
+    const peopleHtmlList = peopleList.map((person) => <p key={person.id}>{person.lastname}</p>)
+    return (
+        <>
+            {peopleHtmlList}
+        </>
+    )
+}
