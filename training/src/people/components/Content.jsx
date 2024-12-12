b3cfdbd8eb2043de6234c2278d2f577357d65d15
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-12-12 12:00:19 +0100

    introduce navigation

--- training/src/people/components/Content.jsx
+++ training/src/people/components/Content.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter } from "react-router-dom"
+import {Routes, Route} from "react-router"
 import "./Content.css"
 import CreatePerson from "./CreatePerson"
 import PeopleList from "./PeopleList"
 export default function Content(){
     return (
         <>
-            <PeopleList />
-            <hr />
-            <CreatePerson />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/list" element={<PeopleList></PeopleList>} />
+                    <Route path="/create" element={<CreatePerson></CreatePerson>} />
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
