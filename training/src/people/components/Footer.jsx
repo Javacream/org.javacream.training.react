70ae9ea3f171d555804467cce7e54972d2e9646d
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-12-12 10:15:37 +0100

    Class Component with state

--- training/src/people/components/Footer.jsx
+++ training/src/people/components/Footer.jsx
@@ -1,8 +1,10 @@
 import "./Footer.css"
 import {company} from '../PeopleContext'
+import ClockClassComponent from "../../util/ClockClassComponent"
 export default function Footer(){
     return (
         <div className="Footer">
+            <ClockClassComponent />
             <p>{company}</p>
         </div>
             )
