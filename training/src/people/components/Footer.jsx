25a0c58057d8e2c48ed5cca47b347cff4f7ecd2c
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-12-12 10:40:58 +0100

    introduce ClockFunctionalComponent

--- training/src/people/components/Footer.jsx
+++ training/src/people/components/Footer.jsx
@@ -1,10 +1,12 @@
 import "./Footer.css"
 import {company} from '../PeopleContext'
 import ClockClassComponent from "../../util/ClockClassComponent"
+import ClockFunctionalComponent from "../../util/ClockFunctionalComponent"
 export default function Footer(){
     return (
         <div className="Footer">
             <ClockClassComponent />
+            <ClockFunctionalComponent />
             <p>{company}</p>
         </div>
             )
---
+++ training/src/util/ClockFunctionalComponent.jsx
@@ -0,0 +1,21 @@
+import {useState, useEffect} from 'react'
+
+export default function ClockFunctionalComponent(){
+    const [actualTime, updateActualTimeFunction] = useState(new Date())
+    // NEVER, NEVER, NEVER!!! 
+    //setInterval(() => updateActualTimeFunction(new Date()), 1000)
+    
+    let interval_callback = () => {updateActualTimeFunction(new Date())}
+    let useEffect_callback = () => {
+        const clock_interval = setInterval(interval_callback, 1000)
+        return () => clearInterval(clock_interval)
+    }
+    
+    useEffect(useEffect_callback, [])
+    
+    return (
+        <>
+            <p>{actualTime.toUTCString()}</p>
+        </>
+    )
+}
