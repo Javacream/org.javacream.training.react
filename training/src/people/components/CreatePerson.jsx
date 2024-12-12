50db82a469c0b179d80213cffb557c99502d80ca
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-12-12 11:49:53 +0100

    finish CreatePerson

--- training/src/people/components/CreatePerson.jsx
+++ training/src/people/components/CreatePerson.jsx
@@ -1,24 +1,29 @@
-import { useState } from "react";
-
+import {useState} from 'react'
+import { peopleModel } from '../PeopleContext'
+import PersonComponent from './PersonComponent'
 export default function CreatePerson(){
-    const [lastname, updateLastname] = useState('')
+    const [lastname, updateLastname] = useState("") 
+    const [firstname, updateFirstname] = useState("") 
+    const [person, updatePerson] = useState({}) 
     const handleSubmit = (event) => {
-        event.preventDefault() //SPA
-        console.log('you entered: '+ lastname)
+        event.preventDefault()
+        const p = peopleModel.create(lastname, firstname, 177, 'd')
+        updatePerson(p)
+        console.log(peopleModel.allPeople())
     }
 
     return (
         <>
             <form onSubmit={handleSubmit}>
                 <label>Lastname:
-                    <input type='text' value={lastname} onChange={(e) =>
-                    {
-                        updateLastname(e.target.value)
-                    }
-                } />
+                    <input type = "text" value={lastname} onChange={(e) => updateLastname(e.target.value)}></input>
                 </label>
-                <input type='submit' value='create' />
+                <label>Firstname:
+                    <input type = "text" value={firstname} onChange={(e) => updateFirstname(e.target.value)}></input>
+                </label>
+                <input type="submit" value="Create Person"/>
             </form>
+            <PersonComponent person={person} detail={true}></PersonComponent>
         </>
     )
 }
