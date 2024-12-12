ea59a8f9299dd5d07556877ac0c6f987faddeb87
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-12-12 15:11:01 +0100

    introduce Redux sample

--- training/src/people/PeopleContext.jsx
+++ training/src/people/PeopleContext.jsx
@@ -1,5 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit"
 import {Person,  PeopleModel } from "./model/PeopleModel"
 import {Subject} from 'rxjs'
+import reducer from '../counter/CounterSlice'
+
 export const applicationTitle = 'PEOPLE'
 export const company = 'Javacream'
 export let peopleModel = new PeopleModel()
@@ -14,4 +17,11 @@ peopleModel.create('Sawitzki', 'Rainer', 183, 'm')
 peopleModel.create('Smith', 'Andrea', 156, 'd')
 export let user = new Person(1000, 'Doe', 'John', 199, 'd')
 setTimeout(() => {user.lastname = 'Smith'}, 1000)
+export const store = configureStore(
+    {
+        reducer: {
+            counter: reducer
+        }
+    }
 
+)
--- training/src/people/components/Content.jsx
+++ training/src/people/components/Content.jsx
@@ -3,15 +3,18 @@ import {Routes, Route} from "react-router"
 import "./Content.css"
 import CreatePerson from "./CreatePerson"
 import PeopleList from "./PeopleList"
+import CounterComponent from '../../counter/CounterComponent'
 export default function Content(){
     return (
         <>
             <BrowserRouter>
                 <Link to="/list">List</Link>
                 <Link to="/create">Create</Link>
+                <Link to="/redux">Redux</Link>
                 <Routes>
                     <Route path="/list" element={<PeopleList></PeopleList>} />
                     <Route path="/create" element={<CreatePerson></CreatePerson>} />
+                    <Route path="/redux" element={<CounterComponent></CounterComponent>} />
                 </Routes>
             </BrowserRouter>
         </>
