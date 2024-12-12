0a874bf8693286c2559f674533822ea07e2252f5
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-12-12 10:58:12 +0100

    Finish functional clock component

--- training/src/util/ClockFunctionalComponent.jsx
+++ training/src/util/ClockFunctionalComponent.jsx
@@ -2,14 +2,11 @@ import {useState, useEffect} from 'react'
 
 export default function ClockFunctionalComponent(){
     const [actualTime, updateActualTimeFunction] = useState(new Date())
-
-    let interval_callback = () => {updateActualTimeFunction(new Date())}
-    let useEffect_callback = () => {
-        const clock_interval = setInterval(interval_callback, 1000)
-        return () => clearInterval(clock_interval)
-    }
     
-    useEffect(useEffect_callback, [])
+    useEffect(() => {
+        const clock_interval = setInterval(() => {updateActualTimeFunction(new Date())}, 1000)
+        return () => clearInterval(clock_interval)
+    }, [])
     
     return (
         <>
